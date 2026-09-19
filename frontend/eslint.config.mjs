80cdcd0895efe31e7a243131094ceefd4cb42feb
Durga Tharshini <durgatharshini11@gmail.com>
2026-09-19 21:33:45 +0530

    chore(frontend): upgrade to Next.js 16

---
+++ frontend/eslint.config.mjs
@@ -0,0 +1,17 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+
+export default defineConfig([
+  ...nextVitals,
+  {
+    rules: {
+      "react-hooks/set-state-in-effect": "off",
+      "react-hooks/purity": "off",
+    },
+  },
+  globalIgnores([
+    ".next/**",
+    "out/**",
+    "node_modules/**",
+  ]),
+]);
